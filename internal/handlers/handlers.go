5224183e7d2fda204a9e28c848bbbd3b63ccf2f4
prayagsingh <prayagsingh03@gmail.com>
2021-09-05 22:31:50 +0530

    code cleanup using session to get the room info in post-reservation form added arrival and departure time in reservation-summary page

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,3 +1,6 @@
+// casting start_date, end_date of type string to time.Time
+// ref: 01/02 03:04:05PM '06 -0700
+// article ref: https://www.pauladamsmith.com/blog/2011/05/go_time.html
 package handlers
 
 import (
@@ -73,6 +76,9 @@ func (m *Repository) Reservations(rw http.ResponseWriter, r *http.Request) {
 	// storing room name to reservation
 	res.Room.RoomName = room.RoomName
 
+	// putting room name to session
+	m.App.Session.Put(r.Context(), "reservation", res)
+
 	// converting time.Time formart to string to serve to html page
 	sd := res.StartDate.Format("2006-01-02")
 	ed := res.EndDate.Format("2006-01-02")
@@ -95,6 +101,13 @@ func (m *Repository) Reservations(rw http.ResponseWriter, r *http.Request) {
 // PostReservations handles the posting of a reservation form
 func (m *Repository) PostReservations(rw http.ResponseWriter, r *http.Request) {
 
+	// pulling out the room name and other details from the session
+	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		helpers.ServerError(rw, errors.New("can't get from session"))
+		return
+	}
+
 	// `name` attribute must be present in `input` element to fetch the values from Form
 	err := r.ParseForm()
 	if err != nil {
@@ -102,35 +115,10 @@ func (m *Repository) PostReservations(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	sd := r.Form.Get("start_date")
-	ed := r.Form.Get("end_date")
-
-	// casting start_date, end_date of type string to time.Time
-	// ref: 01/02 03:04:05PM '06 -0700
-	// article ref: https://www.pauladamsmith.com/blog/2011/05/go_time.html
-	layout := "2006-01-02"
-	startDate, err := time.Parse(layout, sd)
-	if err != nil {
-		helpers.ServerError(rw, err)
-	}
-
-	endDate, err := time.Parse(layout, ed)
-	if err != nil {
-		helpers.ServerError(rw, err)
-	}
-
-	// fetch room-id
-	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
-
-	reservation := models.Reservation{
-		FirstName: r.Form.Get("first_name"),
-		LastName:  r.Form.Get("last_name"),
-		Email:     r.Form.Get("email"),
-		Phone:     r.Form.Get("phone"),
-		StartDate: startDate,
-		EndDate:   endDate,
-		RoomID:    roomID,
-	}
+	reservation.FirstName = r.Form.Get("first_name")
+	reservation.LastName = r.Form.Get("last_name")
+	reservation.Phone = r.Form.Get("phone")
+	reservation.Email = r.Form.Get("email")
 
 	// creating a form object to check our data
 	form := forms.New(r.PostForm)
@@ -141,7 +129,6 @@ func (m *Repository) PostReservations(rw http.ResponseWriter, r *http.Request) {
 	form.IsEmail("email")
 
 	if !form.Valid() {
-
 		data := make(map[string]interface{})
 		data["reservation"] = reservation
 
@@ -159,11 +146,11 @@ func (m *Repository) PostReservations(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	restriction := models.RoomRestriction{
-		RoomID:        roomID,
+		RoomID:        reservation.RoomID,
 		ReservationID: newReservationID,
 		RestrictionID: 1,
-		StartDate:     startDate,
-		EndDate:       endDate,
+		StartDate:     reservation.StartDate,
+		EndDate:       reservation.EndDate,
 	}
 
 	err = m.DB.InserRoomRestriction(restriction)
@@ -297,8 +284,17 @@ func (m *Repository) ReservationSummary(rw http.ResponseWriter, r *http.Request)
 
 	data := make(map[string]interface{})
 	data["reservation"] = reservation
+
+	sd := reservation.StartDate.Format("2006-01-02")
+	ed := reservation.EndDate.Format("2006-01-02")
+
+	stringMap := make(map[string]string)
+	stringMap["start_date"] = sd
+	stringMap["end_date"] = ed
+
 	render.Template(rw, r, "reservation-summary.page.html", &models.TemplateData{
-		Data: data,
+		Data:      data,
+		StringMap: stringMap,
 	})
 }
 
